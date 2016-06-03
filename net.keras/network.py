0c0749b77b0fca16d19261b2a32aef37ca8820e4
Dmitry Karavaev <d.a.karavaev@yandex.ru>
2016-06-03 20:29:05 +0300

    Change again

--- net.keras/network.py
+++ net.keras/network.py
@@ -1,105 +1,110 @@
-import numpy
+import json
+import datetime
+import os
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LeakyReLU, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 
-from data_loader import DataLoader
+from loader import DataLoader
 
-"""
-YOLO Input: Image in RGB
-YOLO Output:
-    SIDE x SIDE x CLASSES:
-        P_{ijk}
-        - Probability of ij-cell has object with k-class
-    SIDE x SIDE x B:
-        scale_{ij0} ... scale_{ijB}
-        - Class scales for each bounding box in ij-cell
-    SIDE x SIDE x B x 4:
-        (x_{ij0}, y_{ij0}, sqrt(h_{ij0}), sqrt(w_{ij0})) ... (x_{ijB}, y_{ijB}, sqrt(h_{ijB}), sqrt(w_{ijB}))
-        - Bounding box definition in each ij-cell
-"""
-
-"""
-print(x_train[0].shape)
-model.add(Convolution2D(512, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Convolution2D(1024, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-
-model.add(Convolution2D(1024, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-
-
-model.add(Convolution2D(512, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-
-img_folder = '/home/dmitry/data/neural-planes/images'
-xml_folder = '/home/dmitry/data/neural-planes/annotations'
-x_train, y_train = list(), list()
-for i in range(1000):
-    x_train.append(numpy.random.uniform(size=shape))
-    y_train.append(numpy.random.uniform(size=637))
-
-model.fit(numpy.asarray(x_train), numpy.asarray(y_train), batch_size=32, nb_epoch=5)
-"""
 
 # TODO: MAKE VALIDATION GENERATOR!
 # TODO: MAKE TEST DATA!
 # TODO: MAKE LOSS FUNCTIONS!
 # TODO: CONVERT PREDICTIONS!
-# TODO: MAKE SINGLE CONFIG!
-# TODO: MODEL WEIGHTS DIRECTORY!
 # TODO: WHY WITH RANDOM DATA ALL WORKS???
 
 
-shape = (3, 224, 224)
+class Network:
+    """
+    Model Input: Image in RGB
+    Model Output:
+        SIDE x SIDE x CLASSES:
+            P_{ijk}
+            - Probability of ij-cell has object with k-class
+        SIDE x SIDE x B:
+            scale_{ij0} ... scale_{ijB}
+            - Class scales for each bounding box in ij-cell
+        SIDE x SIDE x B x 4:
+            (x_{ij0}, y_{ij0}, sqrt(h_{ij0}), sqrt(w_{ij0})) ... (x_{ijB}, y_{ijB}, sqrt(h_{ijB}), sqrt(w_{ijB}))
+            - Bounding box definition in each ij-cell
+        TOTAL = SIDE x SIDE x CLASSES + SIDE x SIDE x B + SIDE x SIDE x B x 4 = SIDE x SIDE x (B x 5 + CLASSES)
+    """
+    def __init__(self, filename):
+        self.config = json.load(open(filename, 'r'))
 
-model = Sequential()
+        self.classes = self.config['global']['model']['classes']
 
-model.add(Convolution2D(16, 3, 3, input_shape=shape))
-model.add(LeakyReLU(alpha=0.1))
-model.add(MaxPooling2D((2, 2)))
+        self.shape = (self.config['global']['image']['channels'],
+                      self.config['global']['image']['size'][0],
+                      self.config['global']['image']['size'][1])
 
-model.add(Convolution2D(32, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-model.add(MaxPooling2D((2, 2)))
+        self.output = (self.config['global']['model']['side'] ** 2) * \
+                      (self.config['global']['model']['boxes'] * 5 + len(self.classes))
 
-model.add(Convolution2D(64, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-model.add(MaxPooling2D((2, 2)))
+        self.lr = self.config['net']['SGD']['rate']
+        self.decay = self.config['net']['SGD']['decay']
+        self.momentum = self.config['net']['SGD']['momentum']
+        self.nesterov = self.config['net']['SGD']['nesterov']
 
-model.add(Convolution2D(128, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-model.add(MaxPooling2D((2, 2)))
+        self.batch = self.config['net']['training']['batch']
+        self.samples = self.config['net']['training']['samples']
+        self.epochs = self.config['net']['training']['epochs']
 
-model.add(Convolution2D(256, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
-model.add(MaxPooling2D((2, 2)))
+        self.model = Sequential()
+        self.form()
 
-model.add(Convolution2D(512, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
+    def form(self):
+        self.model.add(Convolution2D(16, 3, 3, input_shape=self.shape))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(MaxPooling2D((2, 2)))
 
-model.add(Convolution2D(512, 3, 3))
-model.add(LeakyReLU(alpha=0.1))
+        self.model.add(Convolution2D(32, 3, 3))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(MaxPooling2D((2, 2)))
 
-model.add(Flatten())
-model.add(Dense(256))
-model.add(Dense(1024))
-model.add(LeakyReLU(alpha=0.1))
-model.add(Dropout(.5))
-model.add(Dense(49 * (5 * 2 + 3)))
+        self.model.add(Convolution2D(64, 3, 3))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(MaxPooling2D((2, 2)))
 
-sgd = SGD(lr=0.0001, decay=0.0005, momentum=0.9, nesterov=True)
-model.compile(optimizer=sgd, loss='mse')
+        self.model.add(Convolution2D(128, 3, 3))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(MaxPooling2D((2, 2)))
 
-loader = DataLoader('/home/dmitry/data/neural-planes/neural-planes.hdf5')
-history = model.fit_generator(loader.train_flow(chunk=50), samples_per_epoch=10000, nb_epoch=10)
-print(history.history)
-model.save_weights('my_model_weights.h5')
+        self.model.add(Convolution2D(256, 3, 3))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(MaxPooling2D((2, 2)))
 
+        self.model.add(Convolution2D(512, 3, 3))
+        self.model.add(LeakyReLU(alpha=0.1))
 
+        self.model.add(Convolution2D(512, 3, 3))
+        self.model.add(LeakyReLU(alpha=0.1))
+
+        self.model.add(Flatten())
+
+        self.model.add(Dense(256))
+        self.model.add(Dense(1024))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(Dropout(.5))
+
+        self.model.add(Dense(self.output))
+
+        sgd = SGD(lr=self.lr, decay=self.decay, momentum=self.momentum, nesterov=self.nesterov)
+        self.model.compile(optimizer=sgd, loss='mse')
+
+    def train(self):
+        train_loader = DataLoader(os.path.join(self.config['global']['folders']['datasets'],
+                                               self.config['global']['files']['datasets']['train']))
+
+        validation_loader = DataLoader(os.path.join(self.config['global']['folders']['datasets'],
+                                                    self.config['global']['files']['datasets']['train']))
+
+        self.model.fit_generator(train_loader.flow(self.batch), validation_data=validation_loader.flow(self.batch),
+                                 samples_per_epoch=self.samples, nb_epoch=self.epochs, nb_val_samples=2000)
+
+        self.model.save_weights(os.path.join(self.config['global']['folders']['weights'],
+                                             'weights_' + str(datetime.datetime.now()) + '.h5'))
 
